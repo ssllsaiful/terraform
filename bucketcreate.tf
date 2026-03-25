444e2751da6715ea18bafc6e7e57dd506af34b0c
Saiful Islam <ssllsaiful.islam@gmail.com>
2026-03-25 22:58:54 +0600

    test

--- bucketcreate.tf
+++ bucketcreate.tf
@@ -1,15 +1,47 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-west-1" 
 }
 
-resource "aws_s3_bucket" "bucket1" {
-  bucket = "my-unique-bucket-001-xyz"
+# --- Single S3 Bucket ---
+resource "aws_s3_bucket" "jvai-cdn_bucket" {
+
+  bucket = "jvai-cdn_bucket" 
 }
 
-resource "aws_s3_bucket" "bucket2" {
-  bucket = "my-unique-bucket-002-xyz"
+# Allow Public Access (Required for the policy to work)
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket = aws_s3_bucket.cdn_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket" "bucket3" {
-  bucket = "my-unique-bucket-003-xyz"
+# Public Read Policy
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.cdn_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGet"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.cdn_bucket.arn}/*"
+      },
+    ]
+  })
+  # This ensures the 'Public Access Block' is disabled BEFORE the policy is applied
+  depends_on = [aws_s3_bucket_public_access_block.allow_public]
+}
+
+# --- Outputs ---
+output "bucket_name" {
+  value = aws_s3_bucket.cdn_bucket.id
+}
+
+output "bucket_url" {
+  value = "https://${aws_s3_bucket.cdn_bucket.bucket}.s3.us-west-1.amazonaws.com"
 }
